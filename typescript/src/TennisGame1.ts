7625bf5c9ddac4b1cfa895cd6dae39333baa4536
“Nanako <nanako.kono@bizreach.co.jp>
2018-12-18 16:05:41 +0900

    refactor

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,48 +1,66 @@
-import { TennisGame } from './TennisGame';
+import {TennisGame} from './TennisGame';
+
+class Score {
+  player1Score: number = 0;
+  player2Score: number = 0;
+
+  addPlayer1Score (): void {
+    this.player1Score += 1;
+  }
+
+  addPlayer2Score (): void {
+    this.player2Score += 1;
+  }
+
+  isTieScore (): boolean {
+    return this.player1Score === this.player2Score;
+  }
+
+  isWinnerOrAdvantageScore (): boolean {
+    return this.player1Score >= 4 || this.player2Score >= 4;
+  }
+
+  getScoreDiff (): number {
+    return this.player1Score - this.player2Score;
+  }
+}
 
 export class TennisGame1 implements TennisGame {
-  private player1Score: number = 0;
-  private player2Score: number = 0;
+  private playersScore: Score = new Score();
   private player1Name: string;
   private player2Name: string;
 
-  constructor(player1Name: string, player2Name: string) {
+  constructor (
+    player1Name: string,
+    player2Name: string) {
     this.player1Name = player1Name;
     this.player2Name = player2Name;
   }
 
-  wonPoint(playerName: string): void {
+  wonPoint (playerName: string): void {
     if (playerName === 'player1') {
-      this.player1Score += 1;
+      this.playersScore.addPlayer1Score();
       return;
     }
 
-    this.player2Score += 1;
+    this.playersScore.addPlayer2Score()
   }
 
-  getScore(): string {
-    if (this.player1Score === this.player2Score) {
+  getScore (): string {
+    if (this.playersScore.isTieScore()) {
       return this.getTieScoreText();
     }
-    if (this.player1Score >= 4 || this.player2Score >= 4) {
+
+    if (this.playersScore.isWinnerOrAdvantageScore()) {
       return this.getWinnerOrAdvantagePlayerText();
     }
     return this.getScoreText();
   }
 
   private getScoreText (): string {
-    let score = '';
-    let tempScore: number = 0;
-    for (let i = 1; i < 3; i++) {
-      if (i === 1){
-        tempScore = this.player1Score;
-      } else {
-        score += '-';
-        tempScore = this.player2Score;
-      }
-      score += this.convertNumberToTextScore(tempScore);
-    }
-    return score;
+    return this.convertNumberToTextScore(this.playersScore.player1Score) +
+      '-' +
+      this.convertNumberToTextScore(this.playersScore.player2Score);
   }
 
   private convertNumberToTextScore (tempScore: number) {
@@ -59,7 +77,7 @@ export class TennisGame1 implements TennisGame {
   }
 
   private getWinnerOrAdvantagePlayerText (): string {
-    const scoreDiff: number = this.player1Score - this.player2Score;
+    const scoreDiff: number = this.playersScore.getScoreDiff();
     if (scoreDiff === 1) {
       return 'Advantage player1';
     }
@@ -72,8 +90,8 @@ export class TennisGame1 implements TennisGame {
     return 'Win for player2';
   }
 
-  private getTieScoreText(): string {
-    switch (this.player1Score) {
+  private getTieScoreText (): string {
+    switch (this.playersScore.player1Score) {
       case 0:
         return 'Love-All';
       case 1:
